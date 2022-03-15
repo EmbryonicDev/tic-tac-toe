7336368c461bc80014ec2000582340730da029f7
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-03-15 14:43:39 +0100

    Temp: Add event listener to grid cells to display X when clicked

--- script.js
+++ script.js
@@ -8,14 +8,20 @@ const Gameboard = (() => {
 
   // Make board
   const gameboard = document.getElementById('gameboard');
+  gameboard.style.gridTemplateColumns = `repeat(3, 100px)`;
+  gameboard.style.gridAutoRows = `minmax(100px, auto)`;
+  gameboard.style.display = 'grid';
   for (i = 0; i < 9; i++) {
     let cell = document.createElement('div');
     cell.className = 'cell';
     gameboard.append(cell);
+    cell.addEventListener('click', () => {
+      cell.textContent = "X";
+      cell.style.cssText = 'color: red';
+    });
   }
-  gameboard.style.gridTemplateColumns = `repeat(3, 100px)`;
-  gameboard.style.gridAutoRows = `minmax(100px, auto)`;
-  gameboard.style.display = 'grid';
+  
   return { gameboardArr };
+
 
 })();
