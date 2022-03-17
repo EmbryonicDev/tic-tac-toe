11eeb062dec8d6243610b611acc8e8c599acaf23
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-03-17 09:09:55 +0100

    EventListnr for cell -> f playGame, gameboard can now be populated with x / o making turns

--- script.js
+++ script.js
@@ -14,20 +14,45 @@ const Gameboard = (() => {
   for (i = 0; i < 9; i++) {
     let cell = document.createElement('div');
     cell.className = 'cell';
+    cell.dataset.set = [i];
     gameboard.append(cell);
   }
   gameboard.style.gridTemplateColumns = `repeat(3, 100px)`;
   gameboard.style.gridAutoRows = `minmax(100px, auto)`;
   gameboard.style.display = 'grid';
 
-  return { gameboardArr };
+  let moveTracker = 0;
+
+  return { gameboardArr, moveTracker };
 
 })();
 
-document.querySelectorAll(".cell").forEach(item => {
-  item.addEventListener('click', () => {
-    item.textContent = "X";
-    item.style.cssText = 'color: red';
-    console.log('hello')
+const playGame = (() => {
+
+  // Draw X / O on board & fill gameBoardArr
+  document.querySelectorAll(".cell").forEach(item => {
+    item.addEventListener('click', () => {
+      let xMove = "X";
+      let oMove = "O";
+      let tempArr = Gameboard.gameboardArr;
+      let tempMoveTracker = Gameboard.moveTracker;
+      let cellId = item.getAttribute('data-set');
+  
+      if (item.textContent == '') {
+        if (tempMoveTracker == 0 || tempMoveTracker % 2 == 0) {
+          item.textContent = xMove;
+          item.style.cssText = 'color: red';
+          tempArr[cellId] = xMove;
+          tempMoveTracker++;
+        } else if (tempMoveTracker % 2 != 0) {
+          item.textContent = oMove;
+          item.style.cssText = 'color: blue';
+          tempArr[cellId] = oMove;
+          tempMoveTracker++;
+        }
+        Gameboard.moveTracker = tempMoveTracker;
+        Gameboard.gameboardArr = tempArr
+      }
+    })
   })
-})
+})();
