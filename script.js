414cbf3ab66102e698695be710c7e14878a40e23
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-03-21 17:24:55 +0100

    Remove redundant condition from  ScoreBaords (show next player)

--- script.js
+++ script.js
@@ -253,7 +253,7 @@ scoreBoards = () => {
   // Show next player
   if (myVariables.nextToPlay == "O" && myVariables.tie == "") {
     winnerDisplay.innerText = "⭕ to play...";
-  } else if ((myVariables.nextToPlay == "X" || myVariables.nextToPlay == "") && myVariables.tie == "") {
+  } else if (myVariables.nextToPlay == "X" && myVariables.tie == "") {
     winnerDisplay.innerText = "❌ to play...";
   } else if (myVariables.tie == 'Yes') {
     winnerDisplay.innerText = "It's a tie 👔";
